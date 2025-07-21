f12cb54ef0ca43c1981edd394f32cb242b9751d8
nox-vobiscum <noxvobiscum@gmail.com>
2025-07-21 15:53:48 +0200

    new schola2 attempt

---
+++ src/main/java/com/example/schola2/Schola2Application.java
@@ -0,0 +1,13 @@
+package com.example.schola2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schola2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Schola2Application.class, args);
+	}
+
+}
